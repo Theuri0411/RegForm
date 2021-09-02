2fee8bb73e9685bc9f6d152c1e5118b71ffa2ffe
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-09-02 23:56:56 +0300

    first commit and created a form window

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter.font import Font
+root = Tk()
+root.geometry("500x300")
+
+Label(root, text="REGISTRATION FORM", font="ar 15 bold").grid(row=0, column=3)
+
+
+root.mainloop()
