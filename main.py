aab2819ddc023beb0810abace0394e2e171987c1
Theuri0411 <theuri007@students.uonbi.ac.ke>
2021-09-03 00:02:45 +0300

    variable labels added

--- main.py
+++ main.py
@@ -5,5 +5,13 @@ root.geometry("500x300")
 
 Label(root, text="REGISTRATION FORM", font="ar 15 bold").grid(row=0, column=3)
 
+name = Label(root, text="Name")
+phone = Label(root, text="Phone")
+gender = Label(root, text="Gender")
+emergency = Label(root, text="Emergency")
+payment_mode = Label(root, text="Payment Mode")
+
+name.grid(row=, column=2)
+
 
 root.mainloop()
